8a676377d381334f9b2a5fa9e8be38a5ac5ccb03
nehat005 <nehat005@gmail.com>
2022-11-21 16:21:44 +0100

    Modify app script

--- scripts/app/pyannotate_inference.py
+++ scripts/app/pyannotate_inference.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+
 import os
 import sys
 import torch
 import glob
-
+import argparse
 import_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.insert(0, import_path)
 
@@ -13,10 +15,22 @@ print('Using device:', device)
 
 pipeline = torch.hub.load('pyannote/pyannote-audio', 'dia')
 
-if __name__ == '__main__':
-    data_root_path = '/data/SyntheticVerbmobilData/Train/entire_train'
-    save_path = os.path.join(import_path, 'experiments/pyannotate/SyntheticVerbmobil/Train')
+
+def run_pyannotate_inference(data_root_path, save_path):
+    """
+    Runs experiment for all wav files in source path, which also contains reference rttm files and reference scp file
+    as required by dscore evaluation. Stores hypothesis as RTTM's in target path, along with hypothesis scp file
+    Parameters
+    ----------
+    data_root_path: Directory path to wav files
+    save_path: Directory path to store experiment results
+
+    Returns
+    -------
+    None
+    """
     for file in glob.glob(data_root_path + '/*.wav'):
+
         filename = os.path.basename(file).split('.')[0]
         diarization = pipeline({'audio': file})
 
@@ -40,3 +54,18 @@ if __name__ == '__main__':
                                hypothesis_rttm_scpf=os.path.join(save_path,
                                                                  'hyp_rttm.scpf'),
                                save_file_path=os.path.join(save_path, 'relative_evaluation.txt'))
+
+
+def main(args):
+
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--src', help='Source directory path containing wav files', required=False)
+    parser.add_argument('--tgt', help='Target directory path to store experiment results')
+
+    args = parser.parse_args(args)
+
+    run_pyannotate_inference(data_root_path=args.src, save_path=args.tgt)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
